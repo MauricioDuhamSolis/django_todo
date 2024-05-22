ee336b186cc174d0026db04d1b8685045b801830
MauricioDuhamSolis <mauricioduhamsolis@gmail.com>
2024-05-22 02:54:00 -0300

    create Todo model

--- django_todo_api/apps.py
+++ django_todo_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoTodoApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_todo_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_todo_api"
